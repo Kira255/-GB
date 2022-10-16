c67661c529cdc5993a50a02f29ae0470e6e3097c
Kira <kirakulikova255@gmail.com>
2022-10-16 20:45:47 +0300

    Задача 2

---
+++ ex_2/ex_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+
+if (num1 > num2)
+{
+    Console.WriteLine($"max = {num1}");
+}
+else
+{
+    Console.WriteLine($"max = {num2}");
+}
