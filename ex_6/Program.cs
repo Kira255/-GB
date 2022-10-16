39b38362b8f30c6df25310bcf00c87a639e7abcb
Kira <kirakulikova255@gmail.com>
2022-10-16 21:07:47 +0300

    Задача 6

---
+++ ex_6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6. Программа, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Clear();
+
+int num = int.Parse(Console.ReadLine()!);
+
+if (num % 2 == 1 || num % 2 == -1)
+{
+    Console.WriteLine("нет");
+}
+else
+{
+    Console.WriteLine("да");
+}
