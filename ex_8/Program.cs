b7c7de6108cb06d41e364972032632256188827e
Kira <kirakulikova255@gmail.com>
2022-10-16 21:22:32 +0300

    Задача 8

---
+++ ex_8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 8. Программа, которая на вход принимает число(N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+
+int num = int.Parse(Console.ReadLine()!);
+int index = 2;
+
+while (index <= num)
+{
+    Console.WriteLine(index);
+    index = index + 2;
+}
