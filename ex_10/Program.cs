cd36e8fcd76e9e648f5beaf68764514ae602641f
Kira <kirakulikova255@gmail.com>
2022-10-19 22:32:37 +0300

    ex_10

---
+++ ex_10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 10. Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+int num = int.Parse(Console.ReadLine()!);
+num = (num / 10) % 10;
+Console.WriteLine(num);
