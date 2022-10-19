f55a97bc3ea16d0243967c75751917c4d1fbf699
Kira <kirakulikova255@gmail.com>
2022-10-19 22:58:24 +0300

    ex_15

---
+++ ex_15/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 15. Программа, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+
+int num = int.Parse(Console.ReadLine()!);
+if (num >= 6)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
