4093f4137b3993a2d682f94218913ff5168f7360
Kira <kirakulikova255@gmail.com>
2022-10-19 22:53:56 +0300

    ex_13

---
+++ ex_13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13. Программа, которая выводит третью цифру (справа налево) заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+
+int num = int.Parse(Console.ReadLine()!);
+if (num >= 100)
+{
+    while (num >= 1000)
+    {
+        num = num / 10;
+    }
+    num = num % 10;
+    Console.WriteLine(num);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
