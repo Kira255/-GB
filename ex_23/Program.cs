13c503295ff659da5045a333ff8abe6b41569da3
Kira <kirakulikova255@gmail.com>
2022-10-26 21:33:25 +0300

    Ex_23

---
+++ ex_23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 23. Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+string Get(int x)
+{
+    string result = "";
+    for (int i = 1; i < x; i++)
+    {
+        result += (i * i * i).ToString() + ", ";
+    }
+    result += (x * x * x).ToString();
+    return result;
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{Get(N)}");
