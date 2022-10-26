71f1b94ec41eaebe1730371297a5371d8b748e8a
Kira <kirakulikova255@gmail.com>
2022-10-26 21:11:59 +0300

    ex_19

---
+++ ex_19/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+string Check(int x)
+{
+    if ((x / 10000) == (x % 10) && ((x / 1000) % 10) == ((x / 10) % 10))
+    {
+        return "да";
+    }
+    else
+    {
+        return "нет";
+    }
+}
+
+Console.Clear();
+int X = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{Check(X)}");
