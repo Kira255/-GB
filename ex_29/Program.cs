edd63a495bb8ffef9457f5744443c16f89b2ad03
Kira <kirakulikova255@gmail.com>
2022-11-09 22:55:56 +0300

    Ex_29

---
+++ ex_29/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29. Программа, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] FillArray()
+{
+    int[] a = new int[8];
+    for (int i = 0; i < a.Length; i++)
+    {
+        Console.Write("Введите число: ");
+        a[i] = int.Parse(Console.ReadLine()!);
+    }
+    return a;
+}
+
+Console.Clear();
+
+int[] array = FillArray();
+Console.Write(String.Join(", ", array));
+
