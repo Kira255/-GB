fa57a6507159c429d822612bb05cedc64d1d6959
Kira <kirakulikova255@gmail.com>
2022-11-09 22:44:12 +0300

    Ex_27

---
+++ ex_27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27. Программа, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Get(int a)
+{
+    int sum = 0;
+    int i = 0;
+    int b = a;
+    int c = a;
+    while (a > 0)
+    {
+        a = a / 10;
+        i++;
+    }
+    while (i > 0)
+    {
+        b = c % 10;
+        c = c / 10;
+        sum = sum + b;
+        i--;
+    }
+    return sum;
+}
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{Get(A)}");
