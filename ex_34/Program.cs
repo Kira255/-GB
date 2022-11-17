27d0675f4306cd1cf51449d3d2a7ac42adc37d12
Kira <kirakulikova255@gmail.com>
2022-11-17 19:13:32 +0300

    Ex_34

---
+++ ex_34/Program.cs
@@ -0,0 +1,29 @@
+﻿// 
+
+int[] Get(int size)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(100, 1000);
+    }
+    Console.WriteLine($"[{string.Join(", ", res)}]");
+    return res;
+}
+
+int Num(int[] array)
+{
+    int c = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            c++;
+        }
+    }
+    return c;
+}
+
+int[] array = Get(12);
+Console.WriteLine(Num(array));
