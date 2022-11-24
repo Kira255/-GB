aab4ce3cda31abacd143e1ba545668b0854dde18
Kira <kirakulikova255@gmail.com>
2022-11-24 10:12:47 +0300

    Ex_50

---
+++ ex_50/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SquareArray(int[,] inArray, int num)
+{
+    int k = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (inArray[i, j] == num)
+            {
+                Console.WriteLine($"Номер строки: {i + 1}, номер столбца: {j + 1}");
+            }
+            else
+            {
+                k++;
+            }
+        }
+    }
+    if (k > 0)
+    {
+        Console.WriteLine($"такого числа в массиве нет");
+    }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во стобцов: ");
+int cols = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, cols, 1, 10);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Введите число, которое хотите найти в массиве: ");
+int Num = int.Parse(Console.ReadLine()!);
+SquareArray(array, Num);
