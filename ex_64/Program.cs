965d881ad5168d8f24c8572c98371e6370ff7082
Kira <kirakulikova255@gmail.com>
2022-11-30 18:42:18 +0300

    Ex_64

---
+++ ex_64/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+string Rec(int n)
+{
+    if (n <= 1) return "1";
+    else return $"{n}, {Rec(n - 1)}";
+}
+Console.Clear();
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Rec(N));
